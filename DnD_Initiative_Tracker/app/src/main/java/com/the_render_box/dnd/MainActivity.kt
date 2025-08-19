bbc4594a695f7925e7a72d7a97ee115ecbf84dc2
Sid <7568554+unrealsid@users.noreply.github.com>
2025-08-19 12:55:00 +0530

    Initial commit for the project

---
+++ DnD_Initiative_Tracker/app/src/main/java/com/the_render_box/dnd/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.the_render_box.dnd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.the_render_box.dnd.ui.theme.DnD_Initiative_TrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DnD_Initiative_TrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DnD_Initiative_TrackerTheme {
+        Greeting("Android")
+    }
+}
